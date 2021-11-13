51eaecba371b983c336b62ea232b691020cf175a
Arjunser5 <92748603+Arjunser5@users.noreply.github.com>
2021-11-13 23:42:41 +0530

    Update group.js

--- message/group.js
+++ message/group.js
@@ -30,7 +30,7 @@ module.exports = welcome = async (bosco, anu) => {
             }
             hehe = await getBuffer(pp_user)
             if (anu.action == 'add' && mem.includes(bosco.user.jid)) {
-            bosco.sendMessage(anu.jid, `hello bro I am A Bot 😎🌹 To use ${prefix}menu`, 'conversation')
+            bosco.sendMessage(anu.jid, `hello bro I am Arjunser Bot 😎🌹 To use ${prefix}menu`, 'conversation')
             }
              if (anu.action == 'add' && !mem.includes(bosco.user.jid)) {
              const mdata = await bosco.groupMetadata(anu.jid)
